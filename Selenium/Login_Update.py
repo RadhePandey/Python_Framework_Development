9162d2f56a5b7b976f784b363428c64328e6ff84
Radhe <radhepandey08@gmail.com>
2025-08-11 15:53:25 +0530

    Testing by Pahadi

--- Selenium/Login_Update.py
+++ Selenium/Login_Update.py
@@ -4,14 +4,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from Selenium.Login import login_url
-from Selenium.Login import user_name
-from Selenium.Login import password
 
 browser = webdriver.Firefox()
 
 browser.get(login_url)
+browser.minimize_window()
 browser.fullscreen_window()
+
 time.sleep(3)
+browser.forward()
+browser.refresh()
+
+browser.fullscreen_window()
+browser.refresh()
 browser.quit()
 
 
